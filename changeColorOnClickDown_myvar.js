aef232650eb782b17f152d29bfd4ecbbcab46681
arklo <peter.hagerty@virgin.net>
2015-04-06 21:57:28 +0100

    Create changeColorOnClickDown_myvar.js

---
+++ changeColorOnClickDown_myvar.js
@@ -0,0 +1,15 @@
+//  changeColorOnClickDown.js
+// http://business.ozblog.me/scripts/changeColorOnClickDown.js
+
+(function(){ 
+    var clicked = false;
+    this.clickDownOnEntity = function(entityID, mouseEvent) { 
+        if (clicked){
+            Entities.editEntity(entityID, { color: { red: 0, green: 255, blue: 255} });
+            clicked = false;
+        }else{
+            Entities.editEntity(entityID, { color: { red: 255, green: 255, blue: 0} });
+            clicked = true;    
+        }
+    }; 
+})
